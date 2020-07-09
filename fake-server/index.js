40f9a0731cee2786990a77c801251f6f795f69f9
Andrew Gibson <agibson@hublsoft.com>
2020-07-09 16:36:24 +0100

    checking password as well as username

--- fake-server/index.js
+++ fake-server/index.js
@@ -8,14 +8,13 @@ app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    const { username, password } = req.body || {};
-    if (username !== "james") {
-        res.status(403).send("Login failed");
-        console.log("Login failed for", username);
-    }
-    else {
+    let { username, password } = req.body || {};
+    if ((username === "james") && (password = "Password1!")) {
         res.status(201).send("Login succeeded");
         console.log("Login succeeded for", username);
+    } else {
+        res.status(403).send("Login failed");
+        console.log("Login failed for", username);
     }
 });
 
