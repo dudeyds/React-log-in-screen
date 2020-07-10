b6451e75f2536241a81e64c90c7b9d9e6cff26dc
Andrew Gibson <agibson@hublsoft.com>
2020-07-10 15:19:22 +0100

    feedback from review

--- fake-server/index.js
+++ fake-server/index.js
@@ -4,7 +4,6 @@ const fakeUsers = require("./fake-users.json");
 const app = express();
 const port = 3000;
 
-
 function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
@@ -16,24 +15,39 @@ app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post("/", (req, res) => {
+app.post("/login", (req, res) => {
     const { username, password } = req.body || {};
-    if(!username || !password) {
-        res.status(400).send("Bad request");
+    if (!username || !password) {
+        BadRequest(res);
     } else {
-        const user = fakeUsers[username];
-        if(!user) {
-            res.status(403).send("Login failed");
-            console.log("Login failed for unknown user", username);
-        } else {
-            if(password === user.password) {
-                res.status(201).send("Login succeeded");
-                console.log("Login succeeded for", username);
-            } else {
-                res.status(403).send("Login failed");
-                console.log("Login failed for known user", username);
-            }
-        }
+        ProcessValidRequest(res, username, password);
     }
 });
+
+function ProcessValidRequest(res, username, password) {
+    const user = fakeUsers[username];
+    if (!user) {
+        LoginFailed(res);
+        console.log("Login failed for unknown user", username);
+    } else {
+        ProcessKnownUserLogin(password, user, res, username);
+    }
+}
+
+function ProcessKnownUserLogin(res, password, user) {
+    if (password === user.password) {
+        LoginSucceeded(res);
+        console.log("Login succeeded for", user.username);
+    } else {
+        LoginFailed();
+        console.log("Login failed for known user", user.username);
+    }
+}
+
+const LoginSucceeded = res => res.status(201).send("Login succeeded");
+
+const LoginFailed = res => res.status(403).send("Login failed");
+
+const BadRequest = res => res.status(400).send("Bad request");
+
 app.listen(port, () => console.log(`Fake login server listening at http://localhost:${port}`));
