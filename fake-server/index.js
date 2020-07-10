6891e1508cabb2f0090dc81c0739fea208d2979b
Andrew Gibson <agibson@hublsoft.com>
2020-07-10 13:14:44 +0100

    addressed review comments

--- fake-server/index.js
+++ fake-server/index.js
@@ -1,21 +1,38 @@
-const express = require('express')
+const express = require("express")
 const bodyParser = require("body-parser");
+const fakeUsers = require("./fake-users.json");
 const app = express();
-const port = 3000
+const port = 3000;
 
+
+function cors(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+}
+
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    let { username, password } = req.body || {};
-    if ((username === "james") && (password = "Password1!")) {
-        res.status(201).send("Login succeeded");
-        console.log("Login succeeded for", username);
+app.post("/", (req, res) => {
+    const { username, password } = req.body || {};
+    if(!username || !password) {
+        res.status(400).send("Bad request");
     } else {
-        res.status(403).send("Login failed");
-        console.log("Login failed for", username);
+        const user = fakeUsers[username];
+        if(!user) {
+            res.status(403).send("Login failed");
+            console.log("Login failed for unknown user", username);
+        } else {
+            if(user.password === "Password1!") {
+                res.status(201).send("Login succeeded");
+                console.log("Login succeeded for", username);
+            } else {
+                res.status(403).send("Login failed");
+                console.log("Login failed for known user", username);
+            }
+        }
     }
 });
-
 app.listen(port, () => console.log(`Fake login server listening at http://localhost:${port}`));
