11eee36527408a67c26407aae7ea24277ad5235c
Andrew Gibson <agibson@hublsoft.com>
2020-07-10 13:17:37 +0100

    double checked header functionality

--- fake-server/index.js
+++ fake-server/index.js
@@ -8,6 +8,7 @@ const port = 3000;
 function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
+    console.log(req.headers);
     next();
 }
 
